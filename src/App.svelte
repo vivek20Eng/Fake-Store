3ab70529c609e79990b7636eb42970407c5fb985
Vivek s <vivek20sv@gmail.com>
2024-06-22 20:19:09 +0530

    feat: add Carousel component with autoplay and navigation
    - Integrated svelte-carousel for image sliding functionality
    - Added three banners with images for the carousel
    - Enabled infinite looping, autoplay with a duration of 1500ms, and swiping
    - Included a 'Next' button to manually navigate to the next slide

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
+  import FsCarousel from "./components/FSCarousel.svelte";
   import FSHeader from "./components/FSHeader.svelte"
 </script>
 <section class="mx-[5rem]">
   <FSHeader/>
+  <FsCarousel/>
 
 </section>
