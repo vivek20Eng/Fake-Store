66a9c1866d41fb8090e7d1026454d652a37ccd46
Vivek s <vivek20sv@gmail.com>
2024-06-24 11:48:24 +0530

    UI changes

---
+++ src/components/FSFooter.svelte
@@ -0,0 +1,20 @@
+
+
+<footer class="bg-white rounded-lg shadow w-full">
+    <div class="w-full md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://github.com/vivek20Eng/Fake-Store" class="px-12 flex items-center mb-4 sm:mb-0 space-x-3 text-xl rtl:space-x-reverse">
+                <i class="fa-brands fa-github"></i>              <span class="self-center  font-semibold whitespace-nowrap "> vivek </span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 px-12">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Home</a>
+                </li>
+               
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center ">© 2024 <a href="#" class="hover:underline">Fake store</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+
